6efc5fbafdfcacf0111255946b121035d39c91ad
Elvis Chimuse <elvischimuse@gmail.com>
2025-03-21 04:09:32 +0200

    Layout UpdateCreate Product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,20 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Product_Management_System.Data;
+using Product_Management_System.Dto;
+using Product_Management_System.Models;
+using Product_Management_System.Services;
 
 namespace Product_Management_System.Controllers
 {
     public class ProductController : Controller
     {
-        private readonly PmsDbContext _context;
-        private readonly IWebHostEnvironment _hostingEnvironment;
-        public ProductController(PmsDbContext context, IWebHostEnvironment hostingEnvironment)
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
         {
-            _context = context;
-            _hostingEnvironment = hostingEnvironment;
+            _productService = productService;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAll();
+            return View(products);
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDto productDto)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var product = new Product
+                {
+                    ProductName = productDto.ProductName,
+                    ProductDescription = productDto.ProductDescription,
+                    Price = productDto.Price,
+                    ImageFileName = productDto.ImageFile,
+                    IsActive = productDto.IsActive
+                };
+                await _productService.Create(product);
+                return RedirectToAction("Index", "Product");
+            }
+            return View(productDto);
         }
     }
 }
---
+++ Services/ProductService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product_Management_System.Data;
+using Product_Management_System.Models;
+
+namespace Product_Management_System.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly PmsDbContext _context;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public ProductService(PmsDbContext context, IWebHostEnvironment hostingEnvironment)
+        {
+            _context = context;
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public async Task<IEnumerable<Product>> GetAll()
+        {
+            var products = await _context.Products.ToListAsync();
+            return products;
+        }
+
+        public async Task<Product> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> Delete(Product product)
+        {
+            var findProduct = _context.Products.FindAsync(product.Id);
+
+            if(findProduct == null)
+            {
+                return null;
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> FindId(int? id)
+        {
+            var productId = await _context.Products.FindAsync(id);
+
+            if (productId == null)
+            {
+                return null;
+            }
+
+            return productId;
+        }
+
+        public async Task<Product> Update(Product product)
+        {
+            var existingProduct = _context.Products.FindAsync(product.Id);
+
+            if(existingProduct == null)
+            {
+                return null;
+            }
+
+            _context.Entry(existingProduct).CurrentValues.SetValues(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+    }
+}
