5ba564350b5af7a046a165cbb5c4948ef606a401
Elvis Chimuse <elvischimuse@gmail.com>
2025-03-30 03:01:43 +0200

    Clean Up

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -1,12 +1,6 @@
-﻿using System.Net.Mail;
-using System.Net;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
-using SendGrid.Helpers.Mail;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Product_Management_System.Services
 {
